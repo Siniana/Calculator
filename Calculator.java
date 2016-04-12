c104d52504c0e91bea0e63e0256cc3a0c76ea5a9
Alina Böttcher <alina.boettcher@freenet.de>
2016-04-12 10:01:45 +0200

    einlesen eingefügt

--- Calculator.java
+++ Calculator.java
@@ -5,7 +5,10 @@ public class Calculator{
 	public static void main(String[] args){
 		
 		Scanner scanner = new Scanner(System.in);
-		
+		System.out.println("Bitte eingeben [+-*/] zahl1 zahl2:");
+		String operator = scanner .next();
+		int zahl1 = scanner.nextInt();
+		int zahl2 = scanner.nextInt();
 	}
 	
 }
